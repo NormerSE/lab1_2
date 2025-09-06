aec4968efc5670f3d556d8a33426e617102061b8
NormerSE <normer3500@gmail.com>
2025-09-06 17:14:21 +0500

    gitignore и проект Main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# main.py
+
+# Ввод количества чисел
+n = int(input("Введите количество чисел: "))
+
+# Ввод чисел в список (целые числа)
+numbers = []
+for i in range(n):
+    num = int(input(f"Введите число {i+1}: "))
+    numbers.append(num)
+
+# Вывод изначального массива
+print("Изначальный массив:", numbers)
+
+# Сортировка пузырьком по возрастанию
+for i in range(n):
+    for j in range(0, n-i-1):
+        if numbers[j] > numbers[j+1]:
+            numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+
+# Вывод отсортированного массива
+print("Отсортированный массив:", numbers)
+input()
